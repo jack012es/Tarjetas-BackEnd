ffb7a5f277f6bcc89a3b8d4bd492a6df336295a0
jack012es <jacksonmixproject@gmail.com>
2022-02-24 15:07:55 -0500

    Agregar archivos de proyecto.

---
+++ BackEnd/AplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BackEnd.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackEnd
+{
+    public class AplicationDbContext :DbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<TarjetaCredito> TarjetaCredito { get; set; }
+    }
+}
